de248fb9b21f2a1c3df65fd4fce830742d570bb1
Quentin Dufour <quentin@deuxfleurs.fr>
2023-03-14 16:57:12 +0100

    real build.sh file

--- build.sh
+++ build.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-echo hello world
+echo "-- check --"
+nix-shell --attr rust --run "cargo fmt -- --check"
+
+echo "-- build --"
+nix-build --no-build-output --attr clippy.amd64 --argstr git_version $COMMIT
+
+echo "-- unit tests --"
+nix-build --no-build-output --attr test.amd64
+./result/bin/garage_db-*
+./result/bin/garage_api-*
+./result/bin/garage_model-*
+./result/bin/garage_rpc-*
+./result/bin/garage_table-*
+./result/bin/garage_util-*
+./result/bin/garage_web-*
+./result/bin/garage-*
+
+echo "-- integration tests --"
+./result/bin/integration-*
+
+echo "-- smoke tests --"
+rm result
+nix-build --no-build-output --attr clippy.amd64 --argstr git_version $COMMIT
+nix-shell --attr integration --run ./script/test-smoke.sh || (cat /tmp/garage.log; false)
