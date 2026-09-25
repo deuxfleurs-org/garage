f4b06b4fb9282e0838ef04b5087378321ea70d2f
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-09-24 22:40:04 -0400

    docs(cli): fix typo in bucket --owner option

--- src/garage/cli/structs.rs
+++ src/garage/cli/structs.rs
@@ -385,7 +385,7 @@ pub struct PermBucketOpt {
 	#[structopt(long = "write")]
 	pub write: bool,
 
-	/// Allow/deny administrative operations operations
+	/// Allow/deny administrative operations
 	/// (such as deleting bucket or changing bucket website configuration)
 	#[structopt(long = "owner")]
 	pub owner: bool,
